cc5d01bb9f07ae8399708f02ec4dbbd110bb398c
tegarla123@gmail.com <tegarla123@gmail.com>
2024-04-19 10:44:24 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function sayHi(nama){
+  console.log(`Halo bro ${nama}! Ayo kita belajar bareng!`)
+}
+
+const obj = {
+  nama: "Tegar",
+  kelas: "F",
+  angkatan: 2020,
+  IPK: 2.7
+}
+
+sayHi('Tegar')
